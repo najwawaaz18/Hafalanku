931c611ab2bfed22401228564b920f43e176d07a
N <najwawaaz18@gmail.com>
2024-04-04 08:01:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hafalanku/AboutActivity.java
@@ -0,0 +1,6 @@
+package com.example.hafalanku;
+
+import android.app.Activity;
+
+public class AboutActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/hafalanku/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hafalanku; // Replace with your package name
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // You can access your UI elements here (if needed)
+        // For example:
+        // Button loginGuruButton = findViewById(R.id.buttonLoginGuruWaliKelas);
+    }
+}
